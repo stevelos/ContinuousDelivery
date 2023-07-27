8f5d4a81ec883a3c5bc2eb44889522d499a27b80
DivideByZero <48097924+stevelos@users.noreply.github.com>
2023-07-27 08:33:28 +0100

    appsetting greeting

--- ContinuousDelivery/Pages/Index.cshtml.cs
+++ ContinuousDelivery/Pages/Index.cshtml.cs
@@ -1,20 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ContinuousDelivery.Pages
 {
 	public class IndexModel : PageModel
 	{
 		private readonly ILogger<IndexModel> _logger;
+		private readonly IConfiguration _configuration;
 
-		public IndexModel(ILogger<IndexModel> logger)
+		public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
 		{
 			_logger = logger;
+			_configuration = configuration;
 		}
 
 		public void OnGet()
 		{
-
+			//throw new Exception("Testing Insights");
+			ViewData["Greeting"] = _configuration["Greeting"];
 		}
 	}
 }
